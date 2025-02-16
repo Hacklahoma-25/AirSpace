c980a4d631ab436711876464f05c6a0d26107b5a
Gaurav Karwa <gauravkawra41@gmail.com>
2025-02-16 03:57:09 -0600

    added cors headers

--- airspacebackend/airspacebackend/settings.py
+++ airspacebackend/airspacebackend/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mongoapp'
+    'mongoapp',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -48,10 +49,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'airspacebackend.urls'
-
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',  # your Next.js frontend URL
+]
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
