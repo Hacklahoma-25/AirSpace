6bd6660ae9d6cbb658d2fe7307d5de436536efcd
Atharva Lade <ladeatharva@gmail.com>
2025-02-16 08:49:01 -0600

    mongoDB stucture changed

--- airspacebackend/mongoapp/models.py
+++ airspacebackend/mongoapp/models.py
@@ -1,15 +1,21 @@
 from djongo import models
 
 class NFT(models.Model):
-    nft_token = models.CharField(max_length=255)
-    seller_address = models.CharField(max_length=255)
-    nft_name = models.CharField(max_length=255)
-    nft_address = models.CharField(max_length=255)
-    nft_value = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    property_address = models.CharField(max_length=255)
+    current_height = models.CharField(max_length=255)
+    maximum_height = models.CharField(max_length=255)
+    available_floors = models.CharField(max_length=255)
+    price = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    contract_address = models.CharField(max_length=255)
+    token_id = models.IntegerField()
     
     class Meta:
         db_table = 'NFT_information'  # MongoDB collection name
-# from djongo import models
 
 class NFTTransaction(models.Model):
     nft_token_id = models.CharField(max_length=255)
