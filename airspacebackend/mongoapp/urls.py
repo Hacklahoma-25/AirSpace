31898b41ef488a2c95c6495309aa29dbfcc47a38
Gaurav Karwa <gauravkawra41@gmail.com>
2025-02-17 18:40:03 -0600

    removed django

--- airspacebackend/mongoapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import get_all_nfts,create_legal_agreement,analyze_deployment
-
-urlpatterns = [
-    path('nfts/', get_all_nfts, name='get_all_nfts'),
-    path('nft/agreement/', create_legal_agreement, name='create_legal_agreement'),
-    path('analyze_deployment/', analyze_deployment, name='analyze_deployment'),
-]
